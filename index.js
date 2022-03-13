c8aed103e4e6bf0805175004c8bdf16c0f493bd8
CTK WARRIOR <57840201+CTK-WARRIOR@users.noreply.github.com>
2022-03-13 15:24:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cookieStore = new FileCookieStore('./cookies.json')
 const { username, password, tags } = require("./config.json")
 
 const client = new Instagram({ username, password, cookieStore })
-const instaQuote = new Quote({ client, tags, interval: 1000 * 60 })
+const instaQuote = new Quote({ client, tags, interval: 1000 * 60 * 60 * 12 })
 
 client.login().then(async () => {
     console.log("Connected to the instagram :)")
